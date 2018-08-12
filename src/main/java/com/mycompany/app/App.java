9ce6bd6bcc3512c32ccbea4e1404bab5b06f4cc0
SATZKUMR7 <41848546+SATZKUMR7@users.noreply.github.com>
2018-08-12 12:38:37 +0530

    update

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!ready to race!!!!!";
+    private final String message = "Hello World!ready to race!!!!";
 
     public App() {}
 
